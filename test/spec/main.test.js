52aa0a15048bb3b0918aa54ca335a4712389db27
Austin Erlandson <austin@erlandson.com>
2015-02-13 11:30:43 -0600

    finished directory structure and default tests are now passing.

---
+++ test/spec/main.test.js
@@ -0,0 +1,14 @@
+/* jshint mocha: true, expr: true, strict: false, undef: false */
+
+describe('test suite', function () {
+  it('should assert true', function () {
+    true.should.be.true;
+    false.should.be.false;
+  });
+});
+
+describe('hello', function () {
+  it('should return world', function () {
+    hello().should.equal('world');
+  });
+});

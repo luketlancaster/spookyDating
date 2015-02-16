352055dde6343efb8d2825061a2c9652db75441b
jacobdlewis <jacobdlewis@gmail.com>
2015-02-16 01:08:42 -0600

    tests & functions for validating password length and email format

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,30 @@
 /* jshint node: true */
 'use strict';
 
-
-
 //var fb = new Firebase('https://spookydating.firebaseio.com/');
-
-
 
 var hello = function hello() {
   return 'world';
 };
 
-
-
 var profilePicture = $('.profile_picture').val();
 
 function addProfilePic (profilePicture) {
   $('img').attr('src', profilePicture);
+}
+
+function validatePassword (password) {
+  if (password.length > 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validateEmailAddress (emailAddress) {
+  if (emailAddress.substring(emailAddress.length-4) === ".com") {
+    return true;
+  } else {
+    return false;
+  }
 }
--- test/spec/main.test.js
+++ test/spec/main.test.js
@@ -19,69 +19,72 @@ describe('hello', function () {
 //    }
 //});
 
-describe('DOM', function () {
-  describe('form', function () {
-    before(function () {
-      if (window.__karma__) {
-        $('body').append('<form id="registerAndLogin">' +
-                           '<input type="email" class="loginEmail">' +
-                           '<input type="password" class="loginPassword">' +
-                         '</form>');
-      }
-    });
-    it('should exist', function () {
-      $('form').should.exist;
-    });
-
-    describe.skip('', function () {
-      it('', function  () {
-
-      });
-    });
-    describe.skip('newSkaracter', function () {
-      it('on boo click should load a new potential match', function () {
-
-
-      });
-      it('on ooh click should load a new potential match', function () {
-      });
-    });
-
-    //beforeEach(function () {
-      //$('#registerAndLogin').empty();
+//describe('DOM', function () {
+  //describe('form', function () {
+    //before(function () {
+      //if (window.__karma__) {
+        //$('body').append('<form id="registerAndLogin">' +
+                           //'<input type="email" class="loginEmail">' +
+                           //'<input type="password" class="loginPassword">' +
+                         //'</form>');
+      //}
     //});
+    //it('should exist', function () {
+      //$('form').should.exist;
+    //});
+
+    //describe.skip('', function () {
+      //it('', function  () {
+
+      //});
+    //});
+    //describe.skip('newSkaracter', function () {
+      //it('on boo click should load a new potential match', function () {
+
+
+      //});
+      //it('on ooh click should load a new potential match', function () {
+      //});
+    //});
+
+    ////beforeEach(function () {
+      ////$('#registerAndLogin').empty();
+    ////});
+  //});
+//});
+
+
+
+//describe('addProfilePic', function() {
+  //before(function () {
+    //if (window.__karma__) {
+      //$('body').append('<div><img src=""></div>');
+      //}
+    //});
+  //it('should upload a new profile picture', function() {
+    //var profilePicture = 'https://encrypted-tbn1.gstatic.com/' +
+                         //'images?q=tbn:ANd9GcS6H-zrpSy35X405e' +
+                         //'8Ske1fuYB_vQNcNP1pZDkf7mOQirUE9ee6oA';
+    //$('img').attr('src').should.equal('');
+    //addProfilePic(profilePicture);
+    //$('img').attr('src').should.equal(profilePicture);
+  //});
+//});
+
+describe('validatePassword', function() {
+  it('should check that password length is greater than 5 characters', function() {
+   var pword1 = "abc";
+   var pword2 = "abcdefg";
+   validatePassword(pword1).should.be.false;
+   validatePassword(pword2).should.be.true;
   });
 });
 
-
-
-describe('addProfilePic', function() {
-  before(function () {
-    if (window.__karma__) {
-      $('body').append('<div><img src=""></div>');
-      }
-    });
-  it('should upload a new profile picture', function() {
-    var profilePicture = 'https://encrypted-tbn1.gstatic.com/' +
-                         'images?q=tbn:ANd9GcS6H-zrpSy35X405e' +
-                         '8Ske1fuYB_vQNcNP1pZDkf7mOQirUE9ee6oA';
-    $('img').attr('src').should.equal('');
-    addProfilePic(profilePicture);
-    $('img').attr('src').should.equal(profilePicture);
-  });
-});
-
-
-
-describe.skip('addUserData', function() {
-  before(function () {
-    if (window.__karma__) {
-      $('body').append('<div class="blah"></div>');
-      }
-    });
-  it('should append text to page', function() {
-
-    addUserData(name, bio, interests);
-
+describe('validateEmailAddress', function() {
+  it('should verify that email address ends in ".com"', function () {
+    var email1="sscotth@scott.io";
+    var email2="countChocula@gmail.com";
+    validateEmailAddress(email1).should.be.false;
+    validateEmailAddress(email2).should.be.true;
   });
 });

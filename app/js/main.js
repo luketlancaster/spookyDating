922ec8b49913d439729f5af10fe1e8961031a9da
mollytaryn <mollytaryn@gmail.com>
2015-02-15 20:05:53 -0600

    wrote login js

--- app/js/main.js
+++ app/js/main.js
@@ -5,3 +5,76 @@
 var hello = function hello() {
   return 'world';
 };
+
+var $form = $('.form');
+var $tbody = $('.tbody');
+var fbUrl = 'https://spookydating.firebaseio.com';
+var fb = new Firebase(fbUrl);
+var usersFb;
+
+//LOGIN AND MAKE PULL FROM API//
+//usersFb = fb.child('users/' + fb.getAuth().uid + '/data');
+
+//usersFb.once('value', function(data) {
+  //var userInfo = data.val();
+  //Object.keys(userInfo).forEach(function(uuid) {
+
+  //});
+//});
+
+    //$.get(usersFirebaseUrl + '/contact.json?auth=' + fb.getAuth().token, function(data) {
+      //if (data) {
+        //Object.keys(data).forEach(function(uuid){
+          //addRowToTable(uuid, data[uuid]);
+        //});
+      //}
+    //});
+  //}
+
+  //LOGIN FUNCTION//
+  $('#login input[type="button"]').click(function(event) {
+    var email = $('#loginEmail').val();
+    var password = $('#loginPassword').val();
+    var loginData = {email: email, password: password};
+
+    fb.authWithPassword(loginData, function(err, auth) {
+      if (err) {
+        $('.error').text(err);
+      } else {
+        location.reload(true);
+      }
+    });
+  });
+
+  //REGISTER FUNCTION//
+  $('#createUser input[type="button"]').click(function(event) {
+    var email = $('#loginEmail').val();
+    var password = $('#loginPassword').val();
+    var loginData = {email: email, password: password};
+
+    registerAndLogin(loginData, function(err, auth) {
+      if (err) {
+        $('.error').text(err);
+      } else {
+        location.reload(true);
+      }
+    });
+  });
+
+  //REGISTER AND LOGIN FUNCTION//
+  function registerAndLogin(obj, cb) {
+    fb.createUser(obj, function(err) {
+      if (!err) {
+        fb.authWithPassword(obj, function(err, auth) {
+          if (!err) {
+            cb(null, auth);
+          } else {
+            cb(err);
+          }
+        });
+      } else {
+        cb(err);
+      }
+    });
+  }
+
--- test/spec/main.test.js
+++ test/spec/main.test.js
@@ -26,18 +26,32 @@ describe('DOM', function () {
   });
 });
 
-
-
 describe('addProfilePic', function() {
   before(function () {
     if (window.__karma__) {
-      var picture = 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcS6H-zrpSy35X405e8Ske1fuYB_vQNcNP1pZDkf7mOQirUE9ee6oA';
       $('body').append('<div><img src=""></div>')
       }
     });
   it('should upload a new profile picture', function() {
+    var picture = 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcS6H-zrpSy35X405e8Ske1fuYB_vQNcNP1pZDkf7mOQirUE9ee6oA';
     $('img').attr('src').should.equal("");
     addProfilePic();
     $('img').attr('src').should.equal(picture);
   });
 });
+
+describe('', function() {
+  before(function() {
+    if (window.__karma__) {
+      $('body').append('<div class="noMatches"><div class="bio"></div></div>')
+    }
+  });
+  it('should add a bio to the profile', function() {
+    var bio = "My name is monster";
+    $('.bio').should.be('');
+    addUserData();
+    $('.bio').should.equal(bio);
+  });
+});
+
+
